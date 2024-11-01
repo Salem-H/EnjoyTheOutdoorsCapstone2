edfad5dcb5b3154132349d7ad19d9ffec4fc6870
Salem H <salemkassal@gmail.com>
2024-11-01 15:53:36 -0400

    first commit

---
+++ scripts/mountains_page.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const mountainsSelect = document.getElementById("mountainsSelect")
+    mountainsArray.forEach(m => mountainsSelect.appendChild(new Option(m.name)));
+    mountainsSelect.addEventListener("change", e => {
+        const selectedIndex = mountainsSelect.selectedIndex;
+        if(selectedIndex){
+            const m = mountainsArray[selectedIndex - 1];
+            const coords = m.coords.lat.toFixed(3) + ", " + m.coords.lng.toFixed(3)
+            results.innerHTML = `
+            <h1>${m.name}</h1>
+            Elevation: <b>${m.elevation}</b><br> 
+            Effort: <b>${m.effort}</b> <br> 
+            Coordinates: <b>(${coords})</b><br>
+            <br>
+            ${m.desc}
+            <br>
+            `;
+            if(m.img){
+                const i = document.createElement("img");
+                i.alt = "Mountain Image";
+                i.src = "data/images/" + m.img ;
+                results.appendChild(i);
+            }
+        }
+    })
+});
+
+
+/* <img src="data/images/${m.img}" alt="photo"></img> */
